40c0527937eda691217eadf8514bba7b2b33add7
maxschemann <schemann@phonetix.net>
2022-04-04 15:58:19 +0200

    Service implementiert

--- src/main/java/de/neuefische/cgn221springstudent/services/StudentService.java
+++ src/main/java/de/neuefische/cgn221springstudent/services/StudentService.java
@@ -1,5 +1,18 @@
 package de.neuefische.cgn221springstudent.services;
 
-public class StudentService {
+import de.neuefische.cgn221springstudent.model.Student;
+import de.neuefische.cgn221springstudent.repo.StudentRepo;
+import org.springframework.stereotype.Service;
 
+@Service
+public class StudentService {
+    StudentRepo repo = new StudentRepo();
+
+    public void addStudent(Student student) {
+        repo.addStudent(student);
+    }
+
+    public Student getStudentById(int id) {
+        return repo.getStudentById(id);
+    }
 }
