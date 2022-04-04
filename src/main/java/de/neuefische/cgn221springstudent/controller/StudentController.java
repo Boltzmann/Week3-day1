55d9212acb835ff8ad64a3d256da3f7c22ff7fb6
maxschemann <schemann@phonetix.net>
2022-04-04 16:04:44 +0200

    StudentController

--- src/main/java/de/neuefische/cgn221springstudent/controller/StudentController.java
+++ src/main/java/de/neuefische/cgn221springstudent/controller/StudentController.java
@@ -1,18 +1,24 @@
 package de.neuefische.cgn221springstudent.controller;
 
 import de.neuefische.cgn221springstudent.model.Student;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import de.neuefische.cgn221springstudent.services.StudentService;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("student")
 public class StudentController {
-    @GetMapping
-    public List<Student> getAllStudents(){
-        return List.of(new Student("0815", "Emily"), new Student("42", "Lennard"));
+
+    StudentService service = new StudentService();
+
+    @GetMapping(path ="{id}")
+   public Student getStudentById (@PathVariable int id){
+        return service.getStudentById(id);
+    }
+    @PostMapping
+    public void addStudent(@RequestBody Student student){
+        service.addStudent(student);
     }
 }
 
