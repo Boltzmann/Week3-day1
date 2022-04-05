c2265f7dbdb4a80cac81a4525452c98b5370dbcd
Stefan Bollmann <stefan.bollmann@rwth-aachen.de>
2022-04-05 12:55:38 +0200

    Test getAllStudents and addStudent of StudentServiceTest.

--- src/test/java/de/neuefische/cgn221springstudent/services/StudentServiceTest.java
+++ src/test/java/de/neuefische/cgn221springstudent/services/StudentServiceTest.java
@@ -5,6 +5,9 @@ import de.neuefische.cgn221springstudent.repo.StudentRepo;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.mockito.Mockito.*;
 
 class StudentServiceTest {
@@ -16,7 +19,6 @@ class StudentServiceTest {
     void getStudentById() {
         // Given
         when(studentRepo.getStudentById("1")).thenReturn(new Student("1", "Frau Muster"));
-
         // When
         Student expect = new Student("1", "Frau Muster");
         // Then
@@ -25,4 +27,29 @@ class StudentServiceTest {
         verify(studentRepo).getStudentById("1");
         Assertions.assertEquals(expect, actual);
     }
+
+    @Test
+    void addStudent() {
+        // Given done before.
+        // When
+        myTestService.addStudent(new Student("1", "Mister Muster"));
+        // Then
+        verify(studentRepo).addStudent(new Student("1", "Mister Muster"));
+    }
+
+    @Test
+    void getAllStudents() {
+        // Given
+        List<Student> studentList = new ArrayList<>(List.of(
+                new Student("1", "Mister Muster"),
+                new Student("2", "Frau Muster"))
+        );
+        when(studentRepo.getAllStudentsAsList()).thenReturn(studentList);
+        // When - redundant: expect List similar to studentList.
+        // Then
+        List<Student> actual = myTestService.getAllStudents();
+        Assertions.assertEquals(studentList, actual);
+    }
+
+
 }
